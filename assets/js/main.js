06df3c3f3288320f0695497fc174b440744228dc
tatata812 <75353660+tatata812@users.noreply.github.com>
2025-04-13 20:49:36 +0900

    修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,12 +102,12 @@ $(function () {
 })
 
 
-$('.carousel-js').slick({
+$('.about-slider-js').slick({
   autoplay: true, // 自動でスクロール
   autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
   speed: 5000, // スライドが流れる速度を設定
   cssEase: "linear", // スライドの流れ方を等速に設定
-  slidesToShow: 4, // 表示するスライドの数
+  slidesToShow: 2, // 表示するスライドの数
   swipe: false, // 操作による切り替えはさせない
   arrows: false, // 矢印非表示
   pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
